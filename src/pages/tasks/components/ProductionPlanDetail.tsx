3d5157cd192fef1fafdbb7776326875518b6ef88
Mai Huu Hung <maihuuhung2002@gmail.com>
2026-04-17 10:25:22 +0700

    thêm UI cho bang 3

---
+++ src/pages/tasks/components/ProductionPlanDetail.tsx
@@ -0,0 +1,154 @@
+import React from 'react';
+import DataGrid, {
+  Column,
+  Scrolling,
+  FilterRow,
+  HeaderFilter,
+  Selection,
+  Editing,
+} from 'devextreme-react/data-grid';
+
+export function ProductionPlanDetail() {
+
+  return (
+    <div className="grid-container">
+      <DataGrid
+        dataSource={[]} //MockData dữ liệu
+        height={450}
+        showBorders
+        focusedRowEnabled
+        columnAutoWidth
+        wordWrapEnabled
+      >
+        <Scrolling mode="standard" showScrollbar="always" />
+        <FilterRow visible />
+        <HeaderFilter visible />
+        <Selection mode="single" />
+        <Editing mode="cell" allowUpdating={true} />
+
+        {/*Bảng Datagrid*/}
+          <Column 
+          dataField="line" 
+          caption="LINE" 
+          width={80} 
+          allowEditing={false} 
+          />
+
+          <Column 
+          dataField="season" 
+          caption="Season" 
+          width={100} 
+          allowEditing={false} 
+          />
+
+          <Column dataField="style"
+          caption="Style" 
+          width={120} 
+          allowEditing={false} 
+          />
+
+          <Column 
+          dataField="buy" 
+          caption="BUY" 
+          width={110} 
+          allowEditing={false} 
+          />
+
+          <Column 
+          dataField="category" 
+          caption="Category" 
+          width={140} 
+          allowEditing={true} 
+          />
+        
+          <Column 
+            dataField="orderQty" 
+            caption="Order Qty" 
+            dataType="number" 
+            format="#,##0" 
+            width={130} 
+            allowEditing={false} 
+          />
+
+          <Column 
+            dataField="dutyQty" 
+            caption="Duty Qty" 
+            dataType="number" 
+            format="#,##0" 
+            width={110} 
+            allowEditing={true} 
+          />
+
+          <Column 
+            dataField="target" 
+            caption="Target" 
+            dataType="number" 
+            format="#,##0" 
+            width={100} 
+            allowEditing={true} 
+          />
+
+          <Column 
+            dataField="inlineDays" 
+            caption="Inline Days" 
+            dataType="number" 
+            width={120} 
+            allowEditing={false} 
+          />
+
+          <Column 
+            dataField="conversionTime" 
+            caption="Conversion Time" 
+            width={170} 
+            allowEditing={true} 
+          />
+
+          <Column 
+            dataField="inlineDate" 
+            caption="Inline date" 
+            dataType="date" 
+            format="dd/MM/yyyy" 
+            width={130} 
+            allowEditing={true} 
+          />
+
+          <Column 
+            dataField="estOfflineDate" 
+            caption="Est. offline date" 
+            dataType="date" 
+            format="dd/MM/yyyy" 
+            width={150} 
+            allowEditing={false} 
+          />
+
+          <Column 
+            dataField="actualOfflineDate" 
+            caption="Actual offline date" 
+            dataType="date" 
+            format="dd/MM/yyyy" 
+            width={160} 
+            allowEditing={true} 
+          />
+
+          <Column 
+            dataField="finalOfflineDate" 
+            caption="Final offline date" 
+            dataType="date" 
+            format="dd/MM/yyyy" 
+            width={150} 
+            allowEditing={false} 
+          />
+
+          <Column 
+            dataField="crd" 
+            caption="CRD" 
+            dataType="date" 
+            format="dd/MM/yyyy" 
+            width={130} 
+            allowEditing={true} 
+          />
+
+      </DataGrid>
+    </div>
+  );
+}
