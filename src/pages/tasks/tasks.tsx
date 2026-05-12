39e7bf1d30c1bc95d2cd41fce0f30e277ab6bd1a
Mai Huu Hung <maihuuhung2002@gmail.com>
2026-05-12 08:05:20 +0700

    Update Tasks.tsx

--- src/pages/tasks/tasks.tsx
+++ src/pages/tasks/tasks.tsx
@@ -3,41 +3,48 @@ import './tasks.scss';
 import { OrdersTable } from './components/OrdersTable';
 import { LineSchedule } from './components/LineSchedule';
 import { ProductionPlanDetail } from './components/ProductionPlanDetail';
-import './tasks.scss';
+import type { Order } from './data/order.data';
 
 export function Tasks() {
-  const [selectedOrder, setSelectedOrder] = useState<any>(null);
-  const [selectedLineIds, setSelectedLineIds] = useState<string[]>([]);
+  const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
+  const [confirmedLines, setConfirmedLines] = useState<string[]>([]);
 
+  const handleConfirmLines = (lineIds: string[], orderId?: number) => {
+    if(!orderId) return;
+
+  }
   return (
     <div className="tasks-page">
-      <h2 className="page-title">CÔNG CỤ KẾ HOẠCH SẢN XUẤT (UI TEST)</h2>
+      <h2 className="page-title">LẬP KẾ HOẠCH SẢN XUẤT</h2>
 
-      {/* Bảng 1 + Bảng 2 */}
+      {/*Bảng 1 + bảng 2*/}
       <div className="two-column-layout">
         <div className="left-panel">
-          <OrdersTable onSelectOrder={setSelectedOrder} />
+          <h5></h5>
+          <OrdersTable onSelectOrder={setSelectedOrder} />  
         </div>
 
         <div className="right-panel">
+          <h5></h5>
           <LineSchedule
             selectedOrder={selectedOrder}
-            onLineChange={(lineId) =>
-              setSelectedLineIds((prev) => {
-                if (!lineId) return [];
-                const has = prev.includes(lineId);
-                return has ? prev.filter((x) => x !== lineId) : [...prev, lineId];
-              })
-            }
-
+            onConfirmLines={setConfirmedLines}
           />
         </div>
       </div>
 
-      {/* Bảng 3: ProductionPlanDetail */}
-      <div className="section">
-        <ProductionPlanDetail selectedLineIds={selectedLineIds} />
-      </div>
+      {/* Bảng 3, Điều kiện chọn confirm tại bảng 2 mới hiện bảng lên */}
+      {confirmedLines.length > 0 && (
+        <div className="section mt-4">
+          <ProductionPlanDetail
+           selectedLineIds={confirmedLines}/>
+        </div>
+      )}
+      {confirmedLines.length === 0 && (
+        <div className="alert alert-info mt-4 text-center p-4">
+          Vui lòng chọn Line để nhập thông tin và bấm <strong>"Chọn"</strong>
+        </div>
+      )}
     </div>
   );
 }
